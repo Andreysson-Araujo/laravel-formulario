97643e4e0157046eff9ea1905845fae9efcc3632
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-03-28 15:34:57 -0500

    add Title to index

--- resources/views/servidores/index.blade.php
+++ resources/views/servidores/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-
+@section('title', 'Boas Vindas')
 <div class="container">
 <h1>Ola Servidor</h1>
     <a id="op" href="{{ route('servidores.create') }}">Responder questionario</a>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrgaoController;
 use App\Http\Controllers\ServidoresController;
+use App\Http\Controllers\NivelController;
 
 
 
 Route::resource('orgaos', OrgaoController::class);
+Route::resource('niveis', NivelController::class);
 
 Route::get('/', function () {
     return view('welcome');
