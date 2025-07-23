bdd43d206e62973bdeb82853dd3b6fe315983cc2
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2025-07-23 08:32:03 -0500

    model Feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'formulario_id',
+        'user_id',
+        'mensagem'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function formulario()
+    {
+        return $this->belongsTo(Formulario::class);
+    }
 }
